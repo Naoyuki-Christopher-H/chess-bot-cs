50ef0283a107562dfcb8ef41d4d922e02b5f593e
YukiChriss <naoyuki.christopher.higaki@outlook.com>
2025-07-02 20:52:51 +0200

    Add assembly metadata to AssemblyInfo.cs
    This commit introduces several assembly attributes to the `AssemblyInfo.cs` file, including title, description, company, product, and version information. It also sets the assembly to be non-COM-visible and includes theme information for WPF applications, establishing the assembly version and file version as "1.0.0.0".

--- chess-bot-cs/AssemblyInfo.cs
+++ chess-bot-cs/AssemblyInfo.cs
@@ -1,4 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
+
+[assembly: AssemblyTitle("Chess Bot")]
+[assembly: AssemblyDescription("A chess game with AI opponent")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Your Company")]
+[assembly: AssemblyProduct("Chess Bot")]
+[assembly: AssemblyCopyright("Copyright © Your Company")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
@@ -8,3 +21,6 @@ using System.Windows;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
